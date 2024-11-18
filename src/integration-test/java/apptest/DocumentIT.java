a0d307261a76b5a399d053955cbb3cc38a88b4a0
AndreSoderlund <andre.soderlund@b3.se>
2024-11-18 12:04:45 +0100

    moved test to same testclass to ensure order with annotation

--- src/integration-test/java/apptest/DocumentIT.java
+++ src/integration-test/java/apptest/DocumentIT.java
@@ -1,5 +1,6 @@
 package apptest;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.client.MultipartBodyBuilder;
@@ -38,14 +39,235 @@ import static org.springframework.util.ResourceUtils.getFile;
 })
 class DocumentIT extends AbstractAppTest {
 
+	private static final String FILTER_PATH = "/2281/documents/filter";
 	private static final String PATH_SUNDSVALL = "/2281/documents";
 	private static final String PATH_ANGE = "/2282/documents";
 	private static final String REQUEST_FILE = "request.json";
 	private static final String RESPONSE_FILE = "response.json";
 	private static final String RESPONSE_FILE_BINARY = "image.jpg";
 
+	/**
+	 * # includeConfidential - false (default)
+	 * # onlyLatestRevision - false (default)
+	 * # limit - 3
+	 */
 	@Test
-	void test01_createDocument() throws FileNotFoundException {
+	@Order(1)
+	void test01_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * #includeConfidential - true
+	 * # onlyLatestRevision - false (default)
+	 * # limit - 3
+	 */
+	@Test
+	@Order(2)
+	void test02_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - false (default)
+	 * # onlyLatestRevision - true
+	 * # limit - 3
+	 */
+	@Test
+	@Order(3)
+	void test03_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?onlyLatestRevision=true&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # limit - 3
+	 */
+	@Test
+	@Order(4)
+	void test04_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # documentTypes - EMPLOYEE_CERTIFICATE
+	 * # limit - 3
+	 */
+	@Test
+	@Order(5)
+	void test05_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&documentTypes=EMPLOYEE_CERTIFICATE&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # documentTypes - EMPLOYEE_CERTIFICATE,HOLIDAY_EXCHANGE
+	 * # limit - 3
+	 */
+	@Test
+	@Order(6)
+	void test06_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&documentTypes=EMPLOYEE_CERTIFICATE,HOLIDAY_EXCHANGE&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # metaData { key = null, matchesAny = Company B }
+	 * # limit - 3
+	 */
+	@Test
+	@Order(7)
+	void test07_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&metaData[0].matchesAny=Company B&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # metaData { key = null, matchesAny = Company B,Company C }
+	 * # limit - 3
+	 */
+	@Test
+	@Order(8)
+	void test08_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&metaData[0].matchesAny=Company B,Company C&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # metaData { key = null, matchesAll = Company B,Manager }
+	 * # limit - 3
+	 */
+	@Test
+	@Order(9)
+	void test09_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&metaData[0].matchesAll=Startup Y,Analyst&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # metaData { key = EMPLOYEE_UNIT }
+	 * # limit - 3
+	 */
+	@Test
+	@Order(10)
+	void test10_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&metaData[0].key=EMPLOYEE_UNIT&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # metaData1 { key = EMPLOYEE_UNIT }
+	 * # metaData2 { key = EMPLOYEE_TYPE }
+	 * # limit - 3
+	 */
+	@Test
+	@Order(11)
+	void test11_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&metaData[0].key=EMPLOYEE_UNIT&metaData[1].key=EMPLOYEE_TYPE&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # metaData1 { key = EMPLOYEE_UNIT, matchesAny = Company B}
+	 * # limit - 3
+	 */
+	@Test
+	@Order(12)
+	void test12_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&metaData[0].key=EMPLOYEE_UNIT&metaData[0].matchesAny=Company B&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	/**
+	 * # includeConfidential - true
+	 * # onlyLatestRevision - true
+	 * # metaData1 { key = EMPLOYEE_UNIT, matchesAny = Company B}
+	 * # metaData2 { key = EMPLOYEE_TYPE, matchesAny = Developer}
+	 * # limit - 3
+	 */
+	@Test
+	@Order(13)
+	void test13_searchByParameters() {
+		setupCall()
+			.withServicePath(FILTER_PATH + "?includeConfidential=true&onlyLatestRevision=true&metaData[0].key=EMPLOYEE_UNIT&metaData[0].matchesAny=Company B&metaData[1].key=EMPLOYEE_TYPE&metaData[2].matchesAny=Developer&limit=3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	@Order(14)
+	void test14_createDocument() throws FileNotFoundException {
 
 		final var testFile = getFile(this.setupPaths().getTestDirectoryPath() + "image.png");
 		final var multipartBodyBuilder = new MultipartBodyBuilder();
@@ -72,7 +294,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test02_updateDocument() {
+	@Order(15)
+	void test15_updateDocument() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2023-2281-123")
 			.withHttpMethod(PATCH)
@@ -83,7 +306,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test03_readDocument() {
+	@Order(16)
+	void test16_readDocument() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2023-2281-123")
 			.withHttpMethod(GET)
@@ -93,7 +317,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test04_readDocumentConfidentialFail() {
+	@Order(17)
+	void test17_readDocumentConfidentialFail() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2024-2281-999")
 			.withHttpMethod(GET)
@@ -103,7 +328,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test05_readDocumentConfidentialSuccess() {
+	@Order(18)
+	void test18_readDocumentConfidentialSuccess() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2024-2281-999?includeConfidential=true")
 			.withHttpMethod(GET)
@@ -113,7 +339,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test06_readDocumentFile() throws IOException {
+	@Order(19)
+	void test19_readDocumentFile() throws IOException {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2023-2281-123/files/4f0a04af-942d-4ad2-b2d9-151887fc995c")
 			.withHttpMethod(GET)
@@ -123,7 +350,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test07_readDocumentFileConfidentialFail() {
+	@Order(20)
+	void test20_readDocumentFileConfidentialFail() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2024-2281-999/files/bd239ee1-27b8-43e7-bb0d-e4ba09b7220e")
 			.withHttpMethod(GET)
@@ -133,7 +361,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test08_readDocumentFileConfidentialSuccess() throws IOException {
+	@Order(21)
+	void test21_readDocumentFileConfidentialSuccess() throws IOException {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2024-2281-999/files/bd239ee1-27b8-43e7-bb0d-e4ba09b7220e?includeConfidential=true")
 			.withHttpMethod(GET)
@@ -143,7 +372,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test09_search() {
+	@Order(22)
+	void test22_search() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=value-3")
 			.withHttpMethod(GET)
@@ -153,7 +383,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test10_searchConfidential() {
+	@Order(23)
+	void test23_searchConfidential() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=value-3&includeConfidential=true")
 			.withHttpMethod(GET)
@@ -163,7 +394,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test11_searchWithWildCardAndText() {
+	@Order(24)
+	void test24_searchWithWildCardAndText() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=*key2")
 			.withHttpMethod(GET)
@@ -173,7 +405,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test12_searchWithWildCardAndTextConfidential() {
+	@Order(25)
+	void test25_searchWithWildCardAndTextConfidential() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=*key2&includeConfidential=true")
 			.withHttpMethod(GET)
@@ -183,7 +416,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test13_searchWithWildCardOnly() {
+	@Order(26)
+	void test26_searchWithWildCardOnly() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=*&sort=id,desc")
 			.withHttpMethod(GET)
@@ -193,7 +427,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test14_searchInLatestRevisionWithWildCardOnly() {
+	@Order(27)
+	void test27_searchInLatestRevisionWithWildCardOnly() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=*&onlyLatestRevision=true")
 			.withHttpMethod(GET)
@@ -203,7 +438,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test15_searchWithWildCardOnlyConfidential() {
+	@Order(28)
+	void test28_searchWithWildCardOnlyConfidential() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=*&includeConfidential=true")
 			.withHttpMethod(GET)
@@ -213,7 +449,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test16_searchInLatestRevisionWithWildCardOnlyConfidential() {
+	@Order(29)
+	void test29_searchInLatestRevisionWithWildCardOnlyConfidential() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "?query=*&includeConfidential=true&onlyLatestRevision=true")
 			.withHttpMethod(GET)
@@ -223,7 +460,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test17_deleteFile() {
+	@Order(30)
+	void test30_deleteFile() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2024-2281-139/files/abc078aa-9335-4b21-b04c-630e27ade51e")
 			.withHttpMethod(DELETE)
@@ -232,7 +470,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test18_deleteFileNotFound() {
+	@Order(31)
+	void test31_deleteFileNotFound() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2023-2281-123/files/6619a286-a6cc-4001-9f55-945734805e7d") // ID doesn't exist
 			.withHttpMethod(DELETE)
@@ -242,7 +481,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test19_addFileToDocument() throws FileNotFoundException {
+	@Order(32)
+	void test32_addFileToDocument() throws FileNotFoundException {
 		final var testFile = getFile(this.setupPaths().getTestDirectoryPath() + "image.png");
 		final var multipartBodyBuilder = new MultipartBodyBuilder();
 		multipartBodyBuilder.part("documentFile", new FileSystemResource(testFile)).filename(testFile.getName()).contentType(IMAGE_PNG);
@@ -265,7 +505,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test20_updateConfidentialityFlag() {
+	@Order(33)
+	void test33_updateConfidentialityFlag() {
 		setupCall()
 			.withServicePath(PATH_SUNDSVALL + "/2023-2281-123/confidentiality")
 			.withHttpMethod(PATCH)
@@ -276,7 +517,8 @@ class DocumentIT extends AbstractAppTest {
 	}
 
 	@Test
-	void test21_updateDocumentType() {
+	@Order(34)
+	void test34_updateDocumentType() {
 		setupCall()
 			.withServicePath(PATH_ANGE + "/2024-2282-666?includeConfidential=true")
 			.withHttpMethod(PATCH)
